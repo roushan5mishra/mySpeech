60811a1c64741d119c90da91903b2594985ff2f9
Roushan Mishra <roushan5mishra@gmail.com>
2016-05-04 23:59:00 +0530

    Update mySpeech.py
    Added text to speech capabilities..

--- mySpeech.py
+++ mySpeech.py
@@ -1,7 +1,13 @@
 import speech_recognition as sr
+import pyttsx as speak
 
 r = sr.Recognizer()
 m = sr.Microphone()
+
+def speaker(word):
+    engine = speak.init()
+    engine.say(word)
+    engine.runAndWait()
 
 try:
     print("A moment of silence, please...")
@@ -17,15 +23,19 @@ try:
         try:
             # recognize speech using Google Speech Recognition
             value = r.recognize_google(audio)
+            
 
             # we need some special handling here to correctly print unicode characters to standard output
             if str is bytes: # this version of Python uses bytes for strings (Python 2)
+                speaker(value) #speaking that a loud
                 print(u"Google think you said: {}".format(value).encode("utf-8"))
             else: # this version of Python uses unicode for strings (Python 3+)
                 print("Google think you said: {}".format(value))
         except sr.UnknownValueError:
+            speaker("I did not understand it")
             print("Oops! Google didn't catch that")
         except sr.RequestError as e:
             print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
 except KeyboardInterrupt:
+    speaker("Good Bye!")
     pass
